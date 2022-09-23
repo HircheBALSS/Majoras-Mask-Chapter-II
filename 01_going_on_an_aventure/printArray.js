87fccc6327cb788852a28ebcaeb81f796ab8a568
baland_h <baland_h@etna-alternance.net>
2022-09-23 11:53:14 -0400

    remove.js

--- 01_going_on_an_aventure/printArray.js
+++
@@ -1,7 +0,0 @@
-function printArray(array) {
-    console.log('List of destination:');
-    for (var j = 0; j < array.length; j += 1) {
-        console.log("".concat(j + 1, " - ").concat(array[j]));
-    }
-}
-printArray(['Swamp', 'Mountain', 'Ocean', 'Canyon']);
